c6e216113c95a4a7b2e941e72abd03949027a9e3
Trevizoli <rtrevizoli@yahoo.com.br>
2024-06-17 14:37:20 -0300

    Segragation of read method

--- banknotes/try-banknotes.cpp
+++ banknotes/try-banknotes.cpp
@@ -2,34 +2,39 @@
 #include <fstream>
 #include <vector>
 
-std::vector<std::string> read_cases()
+std::vector<std::string> read_file(std::string file_path)
 {
-    std::ifstream file ("./banknotes/test_cases.txt");
+    std::ifstream file (file_path);
 
     if (!file.is_open())
     {
-        std::cout << "ERROR: Test cases file not found." << std::endl;
+        std::cout << "ERROR: file not found (" << file_path << ")." << std::endl;
         return {};
     }
 
-    std::vector<std::string> test_cases = {};
+    std::vector<std::string> lines = {};
 
     for (std::string line; std::getline(file, line);)
     {
         std::cout << line << std::endl;
-        test_cases.push_back(line);
+        lines.push_back(line);
     }
 
-    return test_cases;
+    return lines;
+}
 
+std::vector<std::string> read_cases()
+{
+    return read_file("./banknotes/test_cases.txt");
 }
 
 int main()
 {
     std::vector<std::string> input_cases = read_cases();
+    // std::vector<std::string> expected_outputs = read_expected_outputs();
 
-    for (int i = 0; i < cases.size(); i++)
-        check(input_cases[i], expected_outputs[i]);
+    // for (int i = 0; i < cases.size(); i++)
+    //     check(input_cases[i], expected_outputs[i]);
 
     return 0;
 }
