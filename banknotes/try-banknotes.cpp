355e30812096a333416ba2bf3aa20ffa5f33fa7c
Trevizoli <rtrevizoli@yahoo.com.br>
2024-06-17 15:43:49 -0300

    Log cleaning

--- banknotes/try-banknotes.cpp
+++ banknotes/try-banknotes.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <vector>
 
-std::vector<std::string> read_file(std::string file_path)
+std::vector<int> read_file(std::string file_path)
 {
     std::ifstream file (file_path);
 
@@ -12,73 +12,52 @@ std::vector<std::string> read_file(std::string file_path)
         return {};
     }
 
-    std::vector<std::string> lines = {};
+    std::vector<int> lines = {};
 
     for (std::string line; std::getline(file, line);)
-    {
-        std::cout << line << std::endl;
-        lines.push_back(line);
-    }
+        lines.push_back(std::stoi(line));
 
     return lines;
 }
 
-std::vector<std::string> read_cases()
+std::vector<int> read_cases()
 {
     return read_file("./banknotes/test_cases.txt");
 }
 
+void run(int remaining_amount_to_withdraw = 0)
+{
+    std::vector<int> notes = { 100, 50, 20, 10, 5, 2, 1 };
+    std::vector<int> amount_notes_to_withdraw(notes.size());
+
+    std::cout << remaining_amount_to_withdraw << std::endl;
+
+    for (int i = 0 ; i < notes.size(); i++)
+    {
+        while (remaining_amount_to_withdraw >= notes[i])
+        {
+            remaining_amount_to_withdraw -= notes[i];
+            amount_notes_to_withdraw[i]++;
+        }
+
+        continue;
+    }
+
+    for (int i = 0; i < notes.size(); i++)
+        std::cout << amount_notes_to_withdraw[i] << " nota(s) de R$ " << notes[i] << ",00" << std::endl;
+
+}
+
 int main()
 {
-    std::vector<std::string> input_cases = read_cases();
+    std::vector<int> input_cases = read_cases();
     // std::vector<std::string> expected_outputs = read_expected_outputs();
 
     // for (int i = 0; i < cases.size(); i++)
     //     check(input_cases[i], expected_outputs[i]);
 
-    std::vector<int> notes = { 100, 50, 20, 10, 5, 2, 1 };
-    std::vector<int> amount_notes_to_withdraw(notes.size());
-
-    std::string input = "";
-    std::getline(std::cin, input);
-    int remaining_amount_to_withdraw = std::stoi(input);
-
-    std::cout << remaining_amount_to_withdraw << std::endl;
-
-    std::cout << "RTX | DEBUG | 0" <<std::endl;
-    for (int i = 0 ; i < notes.size(); i++)
-    {
-        std::cout << "RTX | DEBUG | 1" <<std::endl;
-        while (remaining_amount_to_withdraw >= notes[i])
-        {
-            std::cout << "RTX | DEBUG | 2" <<std::endl;
-
-            std::cout << "RTX | DEBUG | 2. remaining_amount_to_withdraw - notes[i] => " 
-                << remaining_amount_to_withdraw << " - "
-                << notes[i]
-                << " (" << remaining_amount_to_withdraw - notes[i] << ")."
-                << std::endl;
-
-            std::cout << "RTX | DEBUG | 3" <<std::endl;
-            remaining_amount_to_withdraw -= notes[i];
-            amount_notes_to_withdraw[i]++;
-
-            std::cout << "RTX | DEBUG | 4" <<std::endl;
-
-        }
-        std::cout << "RTX | DEBUG | 5" <<std::endl;
-        continue;
-    }
-
-    for (int& note : notes)
-        std::cout << note << "\t";
-
-    std::cout << std::endl;
-
-    for (int& amount_note_to_withdraw : amount_notes_to_withdraw)
-        std::cout << amount_note_to_withdraw << "\t";
-
-    std::cout << std::endl;
+    for (int input_case : input_cases)
+        run(input_case);
 
     return 0;
 }
